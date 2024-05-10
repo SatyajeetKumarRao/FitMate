8bf964837feceb15e8a718a6c073adc61901f8ef
ShubhKeshari <shubh1999keshari@gmail.com>
2024-05-10 22:26:35 +0530

    UserDashboard Section

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,16 @@
-import './App.css'
-import { Home } from './components/Home'
-import Navbar from './components/Navbar'
+import "./App.css";
+import { Home } from "./components/Home";
+import Navbar from "./components/Navbar";
+import UserDashboard from "./pages/UserDashboard";
 
 function App() {
-
   return (
     <>
-    {/* <Navbar/> */}
+      {/* <Navbar/> */}
       <Home />
+      {/* <UserDashboard/> */}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
